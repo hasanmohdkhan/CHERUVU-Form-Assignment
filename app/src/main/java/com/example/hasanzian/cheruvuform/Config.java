e805ab4612f675cf84dd8a2b2b5cc3eb0b3edc01
hasanmohdkhan <hasanzian@gmail.com>
2017-04-26 23:25:33 +0530

    yup PPPPPIIIII

---
+++ app/src/main/java/com/example/hasanzian/cheruvuform/Config.java
@@ -0,0 +1,28 @@
+package com.example.hasanzian.cheruvuform;
+
+/**
+ * Created by hasanZian on 26-04-2017.
+ */
+
+public class Config {
+    //Address of our scripts of the CRUD
+    public static final String URL_ADD="http://192.168.43.56/android_connect/addEmp.php ";
+
+
+    //Keys that will be used to send the request to php scripts
+    public static final String KEY_FAR_SNO = "Sno";
+    public static final String KEY_FAR_NAME = "FarmerName";
+    public static final String KEY_FAR_AGE = "FarmerAge";
+    public static final String KEY_FAR_VILLAGE = "Village";
+    public static final String KEY_FAR_MANDAL = "Mandal";
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_DESG = "desg";
+    public static final String TAG_SAL = "salary";
+
+
+
+}
